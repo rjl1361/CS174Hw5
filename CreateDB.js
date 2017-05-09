a2c61b64732655fe4d9800fb9a56859409439f59
Misha <misha.behey@gmail.com>
2017-05-09 15:29:01 -0700

    database should be working fine now

--- CreateDB.js
+++ CreateDB.js
@@ -1,33 +1,33 @@
 var mysql = require('mysql');
-
 var connection = mysql.createConnection( {
-		host: '127.0.0.1',
-		user: 'root',
-		password: '',
-		database: 'notdeadyet'
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'notdeadyet'
 });
 
 connection.connect();
 
+/*connection.query('DROP DATABASE IF EXISTS notdeadyet', function(err, res, fields) {
+    if (err) throw err;
 
+    console.log('database' + connection.database + 'dropped');
+})
+*/
 connection.query('CREATE DATABASE IF NOT EXISTS notdeadyet',
      function (error, results, fields) {
           if (error) throw error;
      }
 );
-
 connection.query('USE notdeadyet', function (error, results, fields) {
    if (error) throw error;
 });
 
-connection.query('CREATE TABLE IF NOT EXISTS USER (ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, LAST_CHECK_IN INTEGER (11) NOT NULL DEFAULT 0, LAST_EMAIL_SENT INTEGER (11) NOT NULL DEFAULT 0, NOTIFY_LIST TEXT, MESSAGE TEXT )',
+connection.query('CREATE TABLE IF NOT EXISTS USER (ID INT (10) NOT NULL AUTO_INCREMENT, LAST_CHECK_IN INT (10) NOT NULL DEFAULT 0, LAST_EMAIL_SENT INT (10) NOT NULL DEFAULT 0, NOTIFY_LIST VARCHAR(100) , MESSAGE VARCHAR(100), PRIMARY KEY (ID) )',
  function(err, results, fields) {
-	if (err) throw err;
+    if (err) throw err;
+
+    console.log(results);
 });
 
-connection.end(function(err){
-	if (err) throw err;
-});
-
-console.log("database created");
-
+connection.end();
