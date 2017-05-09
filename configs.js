93860585bc10253631cc8c7d2693dffb83537c6d
Misha <misha.behey@gmail.com>
2017-05-09 10:21:02 -0700

    updated config

--- configs.js
+++ configs.js
@@ -1,7 +1,14 @@
 var config = {
 	check_in_frequency: 1000,
 	notify_delay: 1000,
-	email_job_frequency: 1000
+	email_job_frequency: 1000,
+			"SECRET_KEY": "INSERT YOUR KEY FROM THE STRIPE.COM", //this probably shouldn't be here :( 
+    "PUBLISHABLE_KEY": "INSERT YOUR KEY FROM THE STRIPE.COM",
+    "CHARGE_URL": "https://api.stripe.com/v1/charges",
+    "CHARGE_CURRENCY": "usd",
+    "CHARGE_DESCRIPTION": "Buyer sees this on their statement",
+    "CHARGE_USERAGENT": "CreditCardTester",
+    "TIMEOUT": 20
 }
 
 module.exports = config;
